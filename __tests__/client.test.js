9fec052ce3165523a38092624a7a9dbd4a09eb6e
Márcio Viegas <marcioaugustoviegas@gmail.com>
2020-07-31 14:08:08 -0300

    add menu

--- __tests__/client.test.js
+++ __tests__/client.test.js
@@ -1,5 +1,32 @@
 import app from '../src/public/app';
 
-test('extractRoversInformation', () => {
-  expect(app('b')).toBe('b');
+const state = {
+  rovers: [
+    {
+      name: 'Curiosity',
+      landing_date: '2012-08-06',
+      launch_date: '2011-11-26',
+      max_date: '2020-07-28',
+      status: 'active',
+    },
+    {
+      name: 'Spirit',
+      landing_date: '2004-01-04',
+      launch_date: '2003-06-10',
+      status: 'complete',
+      max_date: '2010-03-21',
+    },
+    {
+      name: 'Opportunity',
+      landing_date: '2004-01-25',
+      launch_date: '2003-07-07',
+      status: 'complete',
+      max_date: '2018-06-11',
+    },
+  ],
+};
+
+test('build app', () => {
+  const appRendered = app(state);
+  expect(appRendered).toBe('<ul><li id=\'Curiosity\'><a href=\'#\'>Curiosity</a></li><li id=\'Spirit\'><a href=\'#\'>Spirit</a></li><li id=\'Opportunity\'><a href=\'#\'>Opportunity</a></li></ul>');
 });
--- src/public/app.js
+++ src/public/app.js
@@ -1,3 +1,15 @@
-const app = (state) => state;
+const buildMenuItem = (rover) => `<li id='${rover.name}'><a href='#'>${rover.name}</a></li>`;
+
+const wrapperMenu = (menuItemList) => `<ul>${menuItemList.join('')}</ul>`;
+
+const buildMenu = (state) => {
+  const menuItems = state.rovers.reduce((menu, rover) => {
+    menu.push(buildMenuItem(rover));
+    return menu;
+  }, []);
+  return wrapperMenu(menuItems);
+};
+
+const app = (state) => buildMenu(state);
 
 export default app;
